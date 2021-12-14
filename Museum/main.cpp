ea33b8399bc68f0ef7f27b6b08b043c770f1938e
Hen Simkin <hensi1@ac.sce.ac.il>
2021-12-14 19:08:37 +0200

    Add project files.

---
+++ Museum/main.cpp
@@ -0,0 +1,13 @@
+
+#include"Date.h"
+#include"Artist.h"
+#include"Creation.h"
+#include "Picture.h"
+#include"Statue.h"
+#include"Wallstatue.h"
+#include"Museum.h"
+int main()
+{
+	Museum t;
+	t.menu();
+}
